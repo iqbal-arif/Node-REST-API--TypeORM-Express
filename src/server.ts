279534ea0b09828219ca6bd1e4e3ae7f33c184a7
Arif Iqbal <arif_iqbal@yahoo.com>
2023-01-27 18:47:36 -0600

    modified package, server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+/********* FUNCTION SETUP EXPRESS
+Express Routes and Express Middleware
+*/
+function setupExpress() {}
+
+/********* FUNCTION START SERVER
+ Logic to Start Server
+ */
+function startServer() {}
+
+/********* INVOKING THE FUNCTIONS *******/
+setupExpress();
+startServer();
